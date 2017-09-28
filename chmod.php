83a56e359e2444ae93dedb5ff973468861551cf7
chambec <32075060+chambec@users.noreply.github.com>
2017-09-28 15:12:38 -0400

    Create chmod.php

---
+++ chmod.php
@@ -0,0 +1,5 @@
+<?php
+//used when  you can't access via ssh but need to change permissions of
+//a directory or file as long as you can hit this from the web, it should work
+chmod('/home/dir/xxx', 0000)
+?>
